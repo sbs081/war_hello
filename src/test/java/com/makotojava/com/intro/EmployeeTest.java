b05af687975ab30000ffd9b243ad86834d7dfc5d
Aaron Wong <aronwong@Aarons-MacBook-Pro.local>
2017-08-04 19:50:32 +0800

    command alt t

--- src/test/java/com/makotojava/com/intro/EmployeeTest.java
+++ src/test/java/com/makotojava/com/intro/EmployeeTest.java
@@ -10,21 +10,25 @@ import org.junit.Test;
 public class EmployeeTest {
     @Test
     public void test() {
-        int int1 = 1;
-        int int2 = 1;
-        Logger l = Logger.getLogger(EmployeeTest.class.getName());
+        try {
+            int int1 = 1;
+            int int2 = 1;
+            Logger l = Logger.getLogger(EmployeeTest.class.getName());
 
-        l.info("Q: int1 == int2?           A: " + (int1 == int2));
-        Integer integer1 = Integer.valueOf(int1);
-        Integer integer2 = Integer.valueOf(int2);
-        l.info("Q: Integer1 == Integer2?   A: " + (integer1 == integer2));
-        integer1 = new Integer(int1);
-        integer2 = new Integer(int2);
-        l.info("Q: Integer1 == Integer2?   A: " + (integer1 == integer2));
-        Employee employee1 = new Employee("Joe Q", "Author", 42, 173, 82, "Brown", "MALE");
-        Employee employee2 = new Employee("Joe Q", "Author", 42, 173, 82, "Brown", "MALE");
+            l.info("Q: int1 == int2?           A: " + (int1 == int2));
+            Integer integer1 = Integer.valueOf(int1);
+            Integer integer2 = Integer.valueOf(int2);
+            l.info("Q: Integer1 == Integer2?   A: " + (integer1 == integer2));
+            integer1 = new Integer(int1);
+            integer2 = new Integer(int2);
+            l.info("Q: Integer1 == Integer2?   A: " + (integer1 == integer2));
+            Employee employee1 = new Employee("Joe Q", "Author", 42, 173, 82, "Brown", "MALE");
+            Employee employee2 = new Employee("Joe Q", "Author", 42, 173, 82, "Brown", "MALE");
 //        employee1.equals(employee2);
-        Assert.assertEquals(employee1,employee2);
-        l.info("Q: Employee1 == Employee2? A: " + (employee1 == employee2));
+            Assert.assertEquals(employee1,employee2);
+            l.info("Q: Employee1 == Employee2? A: " + (employee1 == employee2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
